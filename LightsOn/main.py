07a2d648a84e6ed7ea8b3c998a0cd974e8de9f23
Saikiran Belana <88756154+belanasaikiran@users.noreply.github.com>
2022-09-11 13:26:20 +0530

    add port input support

--- LightsOn/main.py
+++ LightsOn/main.py
@@ -7,26 +7,23 @@ import pyfirmata
 
 
 # ===============================================
-# Arduino board connected to serial port COM3
-
-# port = input("Select Correct Port: COM")
-# comPort = "COM" + port
-# board = pyfirmata.Arduino("/dev/ttyACM0")
-
+# Input to Arduino board for communication
 
 if __name__ == '__main__':
-    board = pyfirmata.Arduino('/dev/ttyACM0')
+    port = input("Enter COM port Name:  \n")
+    comPort = port
+    board = pyfirmata.Arduino(comPort)
     print("Communication Successfully Initiated ")
 
 # title
 root = Tk()
-root.title("Lights On", font=(18, 'Arial')
-root.geometry("500x400")  # (length x width)
+root.title(" Lights On ")
+root.geometry("600x600")  # (length x width)
 
 
 # ======GET LED PIN ======
-pin='13'
-led=board.digital[13]
+pin = '13'
+led = board.digital[13]
 
 
 # ===============================================
@@ -41,10 +38,14 @@ def Off():
     led.write(0)
 
 
+#  =============================================
+# Button Actions
+# -----------
 Button(root, text="LED ON", width="11", command=On).grid(
     row=1, column=0,   ipadx=2, ipady=2)
 Button(root, text="LED OFF", width="11", command=Off).grid(
     row=1, column=2,   ipadx=2, ipady=2)
 
 
+# ======
 root.mainloop()
